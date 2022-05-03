d4d3c868fa6a25a0145f8d5298b5e3da78b10da9
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-05-03 16:44:08 +0200

    Day 1 complete

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,5 +1,13 @@
-module Day1
-    where
+module Day1 where
 
-solution :: [String] -> String
-solution _ = ""
+solver1 :: [Int] -> Maybe Int
+solver1 [] = Nothing
+solver1 (i:is)
+    | length k == 1 = Just (i * head k)
+    | otherwise = solver1 is
+    where k = filter (\x -> i + x == 2020) is
+
+solver2 :: [Int] -> Maybe Int
+solver2 [] = Nothing
+solver2 input
+    = Just (head [ x*y*z | x <- input, y <- input, z <- input, x + y + z == 2020 ])
--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,25 @@
+import qualified Data.Maybe
+import qualified Day1
 import System.Environment
 import System.IO
-import Day1
 
-dispatch :: [(Int, [String] -> String)]
-dispatch = [
-    (1, Day1.solution)
-    ]
+solve :: ([String] -> Maybe Int) -> [String] -> String
+solve _ [] = "No input provided"
+solve solver input =
+  show $ Data.Maybe.fromMaybe (-1) (solver (map read input))
+
+dispatch :: [(Int, [[String] -> String])]
+dispatch =
+  [ ( 1,
+      [ solve $ Day1.solver1 . map read, -- We want to read these directly to ints
+        solve $ Day1.solver2 . map read
+      ]
+    )
+  ]
 
 main = do
-    input <- getContents
-    (command:_) <- getArgs
-    let Just action = lookup (read command) dispatch
-    print $ action (lines input) ++ "\n"
+  input <- getContents
+  (day : part) <- getArgs
+  let Just solutionList = lookup (read day) dispatch
+      action = solutionList !! (read (head part) - 1) -- Get the part of that day
+  print $ action (lines input)
